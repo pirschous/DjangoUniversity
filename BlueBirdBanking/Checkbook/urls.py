bc7cc492d7e24a2a07e2e0ef95bee5bd71ba594c
pirschous <110510027+pirschous@users.noreply.github.com>
2023-05-05 16:59:07 -0700

    Add files via upload

---
+++ BlueBirdBanking/Checkbook/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # Set the url path to home page index.html.
+    path('', views.home, name='index'),
+    # Sets the url path to Create Neew Account page CreateNewAccount.html.
+    path('create/', views.create_account, name='create'),
+    # Sets the url path to Balance Sheet page BalanceSheet.html.
+    path('<int:pk>/balance/', views.balance, name='balance'),
+    # Sets the url path to Add New Transaction page AddNewTransaction.html.
+    path('transaction/', views.transaction, name='transaction')
+]
+
